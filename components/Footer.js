8f96ff4af1e84acf518e4e516ee32c0fcef039f0
Zenova <109069184+tengkufarhan00@users.noreply.github.com>
2022-07-30 15:27:39 +0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <>
   <footer className="absolute h-[3rem] w-[100%] flex bg-[#1a1a1a] justify-center items-center font-DM text-white">
-  <p>©Made by Zenova, hosted with <a className="font-bold" href="https://netlify.com/">Netlify</a></p>
+  <p>©Made by Zenova, hosted with <a className="font-bold" href="https://vercel.com/">Vercel</a></p>
   </footer>
    </>
   )
