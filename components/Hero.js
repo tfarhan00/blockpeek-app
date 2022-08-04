e1a9d3b0c102bad0332c096df28502bf063423a7
tfarhan <109069184+tengkufarhan00@users.noreply.github.com>
2022-08-04 08:27:11 +0700

    Create Hero.js

---
+++ components/Hero.js
@@ -0,0 +1,14 @@
+
+export default function Hero() {
+  return (
+   <div className="w-[100%] h-[27rem] flex justify-center items-center text-white font-DM">
+   <div className="w-[23rem] h-[23rem] relative  rounded-xl p-5  md:w-[70%] flex justify-center items-center">
+   <div className="text-start flex items-center text-gray-800 font-bold text-[2rem]">
+   <h1>
+   Searching your favorite <em className="text-amber-500">bitcoins</em>, never been this easy.
+   </h1>
+   </div>
+   </div>
+   </div>
+  )
+}
