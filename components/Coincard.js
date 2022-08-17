b229c39b599a6259fd7ece20204e751324f39d3c
tfarhan <109069184+tengkufarhan00@users.noreply.github.com>
2022-08-17 19:42:10 +0700

    udt

--- components/Coincard.js
+++ components/Coincard.js
@@ -80,7 +80,7 @@ export default function Coincard() {
     <i className="bi bi-search"></i>
     </div>
     </div>
-   <div className="md:w-[90%] md:justify-items-center md:grid md:grid-cols-2">
+   <div className="bg-orange-300 md:w-[90%] md:justify-items-center md:grid md:grid-cols-2">
    {filterData ? filterData.map(item => {
      return (
      <>
